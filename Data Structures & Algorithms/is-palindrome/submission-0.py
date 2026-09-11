40be6434982e3dea12f55a486f7a291c519ed67c
cd-ndk <nimaldanyathkbackup@gmail.com>
2026-09-11 23:01:56 +0530

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower()
-        s=''.join(c for c in s if c.isalnum())
-        if s!=s[::-1]:
-            return False
-        return True
+        new_str=""
+        for c in s:
+            if c.isalnum():
+                new_str+=c.lower()
+        return new_str==new_str[::-1]
         
